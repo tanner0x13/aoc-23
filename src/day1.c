8db5846f84eaae2b9d462431d268e972359a2233
Tanner Zigrang <tanner.zigrang@gmail.com>
2023-12-02 21:08:25 -0800

    progressing toward day 1 part 2

--- src/day1.c
+++ src/day1.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+#define MAX_LINE_LENGTH 256
+
+int fpeekc(FILE* fp) {
+    int c = fgetc(fp);
+    ungetc(c, fp);
+    return c;
+}
+
+void get_next_line(FILE *fp, char *buffer) {
+    int i = 0;
+    while(!feof(fp) && i < MAX_LINE_LENGTH - 1) {
+        int c = fgetc(fp);
+        if(c == '\n') { break; }
+        buffer[i++] = (char)c;
+    }
+    buffer[i] = '\0';
+}
+
 int main(int argc, char *argv[]) {
-    //.TODO: command line input validation
+
     // open input file
     const char *filepath = argv[1];
     FILE *fp = fopen(filepath, "r");
@@ -10,30 +28,40 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // initialize
-    int sum = 0;    // where the final answer will go
-    char first = '\0';  // the first digit in a line
-    char last = '\0';   // the last digit in a line
+    // initialize final sum
+    int sum = 0;
 
-    while(!feof(fp)) {
-        // get char
-        char c = fgetc(fp);
-        // is it a digit?
-        if(c >= '0' && c <= '9') {
-            // assign `first` only if it has not already been set this line
-            if(!first) {
-                first = c;
+    // loop through each line in the input file
+    while(fpeekc(fp) != EOF) {
+        // extract the next line from the file
+        char buffer[MAX_LINE_LENGTH];
+        get_next_line(fp, buffer);
+        printf("%s\n", buffer);
+
+        // initialize first/last digit - digits must be positive so negative values represent un-set/invalid
+        int first_digit = -1;
+        int last_digit = -1;
+
+        // loop through string char by char looking for numbers
+        char *curr = &buffer[0];
+        while(*curr != '\0') {
+            if(*curr >= '0' && *curr <= '9') {
+                // case 1: the char is actually 0-9
+                int val = *curr - '0';
+                if(first_digit < 0) { first_digit = val; }
+                last_digit = val;
+                curr++;
+            } else if(0) {
+                // case 2: the char is the beginning of a substr spelling out a number (i.e. 'one')
+
+            } else {
+                // case 3: nothing to see here, move along
+                curr++;
             }
-            // keep updating last, whatever is in `last` when \n is reached is the last digit in the line
-            last = c;   
-        } else if(c == '\n') {
-            // line over - turn two digits into an and add to sum
-            printf("eol: %c%c\n", first, last);
-            sum += (10 * (int)(first - '0')) + (int)(last - '0');
-            // reset state for next line
-            first = '\0';
-            last = '\0';
         }
+
+        // combine digits and add to sum
+        sum += (10 * first_digit) + last_digit;
     }
 
     // print answer and return
