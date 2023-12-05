a439f73fce65059428201af0086720a74f8c4b29
Tanner Zigrang <tanner.zigrang@gmail.com>
2023-12-05 00:05:19 -0800

    day 2 part 1

---
+++ src/day2.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 256
+#define MAX_PULLS_PER_GAME 8
+
+typedef struct {
+    int red;
+    int green;
+    int blue;
+} pull_t;
+
+typedef struct {
+    int gameId;
+    int numPulls;
+    pull_t pulls[MAX_PULLS_PER_GAME];
+} game_t;
+
+void get_next_line(FILE *fp, char *buffer) {
+    int i = 0;
+    while(!feof(fp) && i < MAX_LINE_LENGTH - 1) {
+        int c = fgetc(fp);
+        if(c == '\n') { break; }
+        buffer[i++] = (char)c;
+    }
+    buffer[i] = '\0';
+}
+
+int fpeekc(FILE *fp) {
+    int c = fgetc(fp);
+    ungetc(c, fp);
+    return c;
+}
+
+
+void tokenize(const char *line, game_t *game) {
+    const char *curr = line;
+    // parse out game id
+    char gameIdStr[4] = {0};  // input has 100\0 games
+    curr += 5;  // advance past 'Game ' string
+    for(int i = 0; isdigit(*curr); curr++, i++) {
+        gameIdStr[i] = *curr;
+    }
+    curr += 2;  // advance past ': ' string
+    game->gameId = atoi(gameIdStr);
+
+    // outer loop to parse out each cube pull
+    do {
+        // inner loop to parse out each 'number color' combo
+        while(1) {
+            // read number
+            char numberStr[4] = {0};   // assume no 4 digit+ numbers
+            for(int i = 0; isdigit(*curr); curr++, i++) {
+                numberStr[i] = *curr;
+            }
+            curr++; // advance past space between number and color
+
+            // read color
+            char colorStr[6] = {0};   // green\0 is longest
+            for(int i = 0; isalpha(*curr); curr++, i++) {
+                colorStr[i] = *curr;
+            }
+
+            // write to game struct
+            if(strcmp(colorStr, "red") == 0) {
+                game->pulls[game->numPulls].red = atoi(numberStr);
+            }
+            if(strcmp(colorStr, "green") == 0) {
+                game->pulls[game->numPulls].green = atoi(numberStr);
+            }
+            if(strcmp(colorStr, "blue") == 0) {
+                game->pulls[game->numPulls].blue = atoi(numberStr);
+            }
+
+            if(*curr == ',') {
+                curr+= 2;   // advance past ', ' to next number/color pair
+            } else if (*curr == ';') {
+                curr+= 2;   // advance past '; ' to next number/color pair and break out of inner loop to setup new pull
+                break;
+            } else if(*curr == '\0') {
+                break;      // break out of inner loop but do not advance beyond string bounds
+            } else {
+                printf("unreachable\n");
+            }
+        }
+
+        // increment number of pulls parsed in game struct
+        game->numPulls++;
+    } while(*curr != '\0');
+}
+
+void print_game(const game_t *game) {
+    printf("id: %d, num pulls: %d\n", game->gameId, game->numPulls);
+    for(int i = 0; i < game->numPulls; i++) {
+        printf("\tred: %d, green: %d, blue: %d\n",  game->pulls[i].red, game->pulls[i].green, game->pulls[i].blue);
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    if(argc != 2) {
+        printf("Usage: %s path/to/input\n", argv[0]);
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if(!fp) {
+        printf("failed to open input file %s\n", argv[1]);
+        return 1;
+    }
+
+    int sum = 0;
+
+    while(fpeekc(fp) != EOF) {
+        char lineBuf[MAX_LINE_LENGTH];
+        get_next_line(fp, lineBuf);
+        printf("%s\n", lineBuf);
+        game_t game = {0};
+        tokenize(lineBuf, &game);
+        print_game(&game);
+        int possible = 1;
+        for(int i = 0; i < game.numPulls; i++) {
+            if(game.pulls[i].red > 12 || game.pulls[i].green > 13 || game.pulls[i].blue > 14) {
+                possible = 0;
+                break;
+            }
+        }
+        if(possible) {
+            sum += game.gameId;
+        }
+    }
+
+    printf("Answer: %d\n", sum);
+    fclose(fp);
+    return 0;
+}
